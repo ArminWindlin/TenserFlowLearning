2a984e68a818d9b6c00756fb30559604013e801a
Armin Windlin GHR-OSA-NEX-35 <armin.windlin@swisscom.com>
2018-10-08 17:11:54 +0200

    m

--- StaticTensorFlow/main.js
+++ StaticTensorFlow/main.js
@@ -1,4 +1,75 @@
-test2();
+test3();
+
+function test2() {
+    const model = tf.sequential();
+    //model.add(hidden); // add the layer
+    model.add(tf.layers.dense({units: 2, inputShape: [1]}));
+    model.add(tf.layers.dense({units: 1}));
+
+// Prepare the model for training: Specify the loss and the optimizer.
+    model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+// Generate some synthetic data for training.
+    const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+    const ys = tf.tensor2d([2, 4, 6, 8], [4, 1]);
+
+// Train the model using the data.
+    const configF = {
+        shuffle: true,
+        epochs: 100,
+        callbacks: {
+            onEpochEnd: (epoch, log) => {
+                console.log(`Epoch ${epoch}: loss = ${log.loss}`);
+                model.predict(tf.tensor2d([333], [1, 1])).print();
+            }
+        }
+    };
+
+    hey().then(() => {
+        console.log('done');
+    });
+
+    async function hey() {
+        const response = await model.fit(xs, ys, configF);
+    }
+}
+
+function test3() {
+    const model = tf.sequential();
+    //model.add(hidden); // add the layer
+    model.add(tf.layers.dense({units: 5, inputShape: [2]}));
+    model.add(tf.layers.dense({units: 2}));
+
+// Prepare the model for training: Specify the loss and the optimizer.
+    const sgdOpt = tf.train.sgd(0.001);
+    model.compile({loss: 'meanSquaredError', optimizer: sgdOpt});
+
+// Generate some synthetic data for training.
+    const xs = tf.tensor2d([1, 2, 3, 9, 4, 4, 15, 40], [4, 2]);
+    xs.print();
+    const ys = tf.tensor2d([2, 1, 9, 3, 4, 4, 40, 15], [4, 2]);
+    ys.print();
+
+// Train the model using the data.
+    const configF = {
+        shuffle: false,
+        epochs: 100,
+        callbacks: {
+            onEpochEnd: (epoch, log) => {
+                console.log(`Epoch ${epoch}: loss = ${log.loss}`);
+                model.predict(tf.tensor2d([4, 12], [1, 2])).print();
+            }
+        }
+    };
+
+    hey().then(() => {
+        console.log('done');
+    });
+
+    async function hey() {
+        const response = await model.fit(xs, ys, configF);
+    }
+}
 
 function test() {
     const values = [];
@@ -59,39 +130,5 @@ function test() {
             const response = await model.fit(xs, ys, configF);
             console.log(response.history.loss[0]);
         }
-    }
-}
-
-function test2() {
-    const model = tf.sequential();
-    //model.add(hidden); // add the layer
-    model.add(tf.layers.dense({units: 2, inputShape: [1]}));
-    model.add(tf.layers.dense({units: 1}));
-
-// Prepare the model for training: Specify the loss and the optimizer.
-    model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
-// Generate some synthetic data for training.
-    const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-    const ys = tf.tensor2d([2, 4, 6, 8], [4, 1]);
-
-// Train the model using the data.
-    const configF = {
-        shuffle: true,
-        epochs: 100,
-        callbacks: {
-            onEpochEnd: (epoch, log) => {
-                console.log(`Epoch ${epoch}: loss = ${log.loss}`);
-                model.predict(tf.tensor2d([1234], [1, 1])).print();
-            }
-        }
-    };
-
-    hey().then(() => {
-        console.log('done');
-    });
-
-    async function hey() {
-        const response = await model.fit(xs, ys, configF);
     }
 }
